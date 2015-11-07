bf423ca773730d117cd44ee4e6ed7eacaf096766
Anthony Cook <github@anthonymcook.com>
2015-11-07 04:26:19 -0600

    Promoting get op to metaop since it evals.

--- src/github.com/acook/blacklight/lexer.go
+++ src/github.com/acook/blacklight/lexer.go
@@ -6,7 +6,7 @@ var keywords = []string{
 	"<>", "decap", "depth", "drop", "dup", "over", "rot", "swap", "purge",
 	"newq", "deq", "enq", "q-to-v", "q-to-cv",
 	"s-new", "pop", "push", "size", "tail",
-	"o-new", "child", "self", "get", "set", "fetch",
+	"o-new", "child", "self", "set", "fetch",
 	"''", "()", "v-new", "app", "cat", "ato", "rmo", "len", "v-to-s", "v-to-q",
 	"add", "sub", "mul", "div", "mod", "n-to-c", "n-to-cv",
 	"c-to-n", "c-to-cv",
@@ -19,7 +19,7 @@ var keywords = []string{
 
 var metaops = []string{
 	"@", "$", "^", "$decap", "$drop", "$new", "$swap",
-	"eval",
+	"eval", "get",
 	"if", "either",
 	"until", "while", "loop", "proq",
 	"do", "co", "work", "bkg", "wait",
