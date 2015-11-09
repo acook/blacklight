b9a9027d5d237ab1017dab30b4871a995405a058
Anthony Cook <github@anthonymcook.com>
2015-11-08 23:17:44 -0600

    Adding basic V ops to WV.

--- src/github.com/acook/blacklight/wordvector.go
+++ src/github.com/acook/blacklight/wordvector.go
@@ -16,6 +16,34 @@ func NewWordVector(ops []operation) WordVector {
 	return wv
 }
 
+func (wv WordVector) Cat(wv2 vector) vector {
+	wv.Data = append(wv.Data, wv2.Value().([]Word)...)
+	return wv
+}
+
+func (wv WordVector) App(i datatypes) vector {
+	wv.Data = append(wv.Data, i.(Word))
+	return wv
+}
+
+func (wv WordVector) Ato(n int) datatypes {
+	i := wv.Data[n]
+	return i
+}
+
+func (wv WordVector) Rmo(n int) vector {
+	wv.Data = append(wv.Data[:n], wv.Data[:n]...)
+	return wv
+}
+
+func (wv WordVector) Call(meta *MetaStack) {
+	doEval(meta, wv.Ops)
+}
+
+func (wv WordVector) Len() int {
+	return len(wv.Data)
+}
+
 func (wv WordVector) Value() interface{} {
 	return wv.Data
 }
@@ -30,8 +58,4 @@ func (wv WordVector) String() string {
 		str = str[:len(str)-1]
 	}
 	return str + ")"
-}
-
-func (wv WordVector) Call(meta *MetaStack) {
-	doEval(meta, wv.Ops)
 }
