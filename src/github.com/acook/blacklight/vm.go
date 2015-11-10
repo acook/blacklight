425f405a9d3665538b8ee15ac33ccb85eb07d134
Anthony Cook <github@anthonymcook.com>
2015-11-10 13:27:20 -0600

    VM reads integers, only outputs debug info.

--- src/github.com/acook/blacklight/vm.go
+++ src/github.com/acook/blacklight/vm.go
@@ -6,13 +6,51 @@ import (
 )
 
 func vm(bc []byte) {
-	if bc[0] == 0xF4 {
-		buf := bc[1:8]
-		n, _ := binary.Varint(buf)
-		print(n)
-	} else {
+	var offset, l uint64
+	var b byte
 
-		fmt.Printf("%b ", bc)
+	l = uint64(len(bc))
 
+	for {
+		b = bc[offset]
+
+		if b == 0xF4 {
+			// Integer
+			buf := bc[offset+1 : offset+9]
+			n := Varint64(buf)
+			print(" -- number at offset #" + fmt.Sprint(offset) + ": ")
+			fmt.Printf("%#v\n", n)
+			offset = offset + 8
+		} else {
+			// UNKNOWN
+			print(" -- UNKNOWN at offset #" + fmt.Sprint(offset) + ": ")
+			fmt.Printf("x%x ", b)
+			print("\n")
+		}
+
+		offset++
+		if offset >= l {
+			return
+		}
 	}
+}
+
+func Varint32(buf []byte) int32 {
+	ux := binary.BigEndian.Uint32(buf)
+
+	x := int32(ux >> 1)
+	if ux&1 != 0 {
+		x = ^x
+	}
+	return x
+}
+
+func Varint64(buf []byte) int64 {
+	ux := binary.BigEndian.Uint64(buf)
+
+	x := int64(ux >> 1)
+	if ux&1 != 0 {
+		x = ^x
+	}
+	return x
 }
