6d9f420de651a7b5fd134377beab88227f6ef459
Anthony Cook <github@anthonymcook.com>
2015-11-11 14:47:41 -0600

    Creating a VMstate object to serve as an environment.

---
+++ src/github.com/acook/blacklight/vm_state.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+type VMstate struct {
+	o        uint64 // offset
+	l        uint64 // total length of bc
+	b        byte   // current byte
+	bc       []byte // all bytecode
+	m        *Meta  // meta stack
+	all_map  map[byte]string
+	all_flag bool
+}
+
+func (vm *VMstate) debug() {
+	print(" -- VM STATE\n")
+
+	print("    length: ")
+	print(vm.l)
+	print("\n")
+
+	print("    offset: ")
+	print(vm.o)
+	print("\n")
+
+	print("    current byte: ")
+	print(vm.b)
+	print("\n")
+
+	print("    current inferred: ")
+	print(vm.infer())
+	print("\n")
+
+	print("    all bytes: ")
+	fmt.Printf("x%x", vm.b)
+	print("\n")
+
+	print("    meta stack: ")
+	print(vm.m.String())
+	print("\n")
+}
+
+func (vm *VMstate) infer() string {
+	vm.prepare_lookup()
+
+	return vm.all_map[vm.b]
+}
+
+func (vm *VMstate) prepare_lookup() {
+	vm.all_map = make(map[byte]string)
+
+	if !vm.all_flag {
+		for k, v := range lk_map {
+			vm.all_map[k] = v
+		}
+
+		for k, v := range inb_map {
+			vm.all_map[v] = k
+		}
+
+		vm.all_flag = true
+	}
+}
