83e91074cc0b5a91fe6b4496dc20b12de8d869a7
Anthony Cook <github@anthonymcook.com>
2016-09-10 23:10:00 -0500

    Adding test timer.

--- run_all_examples.bash
+++ run_all_examples.bash
@@ -2,6 +2,24 @@
 
 source gg 2> /dev/null
 
+function timer() {
+  date +%s.%N
+}
+
+function elapsed() {
+  started_at=$1
+  ended_at=$2
+  dt=$(echo "$ended_at - $started_at" | bc)
+  dd=$(echo "$dt/86400" | bc)
+  dt2=$(echo "$dt-86400*$dd" | bc)
+  dh=$(echo "$dt2/3600" | bc)
+  dt3=$(echo "$dt2-3600*$dh" | bc)
+  dm=$(echo "$dt3/60" | bc)
+  ds=$(echo "$dt3-60*$dm" | bc)
+
+  printf " -- time elapsed: %d:%02d:%02d:%02.4f\n" $dd $dh $dm $ds
+}
+
 timestamp="$(date --utc "+%Y.%m.%d")"
 shortsha="$(git rev-parse --short HEAD)"
 blacklight="./bin/blacklight_$shortsha-$timestamp"
@@ -19,6 +37,7 @@ fi
 
 failures=0
 examples=$(find ./examples -maxdepth 1 -regextype posix-extended -regex '\./.*/[a-z].*.bl' -type f -printf '%p\n')
+started_at=$(timer)
 
 for file in $examples; do
   echo
@@ -34,5 +53,9 @@ done
 echo
 echo " -- failures: $failures"
 echo " -- failed: ${failed-<all passed>}"
+
+ended_at=$(timer)
+
+elapsed $started_at $ended_at
 
 exit $failures
