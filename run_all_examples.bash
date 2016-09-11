8c386d5f7b8dc336a60220ad71e2533f2f5f6679
Anthony Cook <github@anthonymcook.com>
2016-09-10 23:05:34 -0500

    Build binary for faster test runs.

--- run_all_examples.bash
+++ run_all_examples.bash
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
 source gg 2> /dev/null
-files=$(find ./examples -maxdepth 1 -regextype posix-extended -regex '\./.*/[a-z].*.bl' -type f -printf '%p\n')
+
+timestamp="$(date --utc "+%Y.%m.%d")"
+shortsha="$(git rev-parse --short HEAD)"
+blacklight="./bin/blacklight_$shortsha-$timestamp"
+
+echo " -- building blacklight binary..."
+go build -o "$blacklight" src/github.com/acook/blacklight/*.go
+
+if [[ -x $blacklight ]]; then
+  echo " -- binary built at: \"$blacklight\""
+else
+  echo " -- something went wrong!"
+  echo " -- binary not found at: \"$blacklight\""
+  exit -1
+fi
 
 failures=0
+examples=$(find ./examples -maxdepth 1 -regextype posix-extended -regex '\./.*/[a-z].*.bl' -type f -printf '%p\n')
 
-for file in $files; do
+for file in $examples; do
   echo
   echo " -- running: $file"
-  echo test | go run src/github.com/acook/blacklight/*.go $file
+  echo test | $blacklight $file
 
   if [ $? -ne 0 ]; then
     failures=$(($failures + 1))
