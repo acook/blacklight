ae07110c60927354550d953f439741d91c2f3c1d
Anthony Cook <github@anthonymcook.com>
2016-09-11 20:55:00 -0500

    Adding CI script.

---
+++ scripts/ci.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+thisscript="ci"
+source "$(dirname ${BASH_SOURCE[0]})/_shared.bash"
+
+if [[ ! -x $(which go) ]]; then
+  warn " -- can't find Go binary in PATH!"
+  warn " -- PATH: $PATH"
+fi
+
+blacklight="$(./scripts/build_test.bash)"
+time ./scripts/run_all_examples.bash $blacklight
