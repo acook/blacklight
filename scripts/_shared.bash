c2ad3c5541f95c21c82b23b301678e413b9f542d
Anthony Cook <github@anthonymcook.com>
2016-09-11 21:02:00 -0500

    Test for bc and just display the times if unavailable.

--- scripts/_shared.bash
+++ scripts/_shared.bash
@@ -31,15 +31,21 @@ function timer() { date "+%s.%N"; }
 function elapsed() {
   started_at=$1
   ended_at=$2
-  dt=$(echo "$ended_at - $started_at" | bc)
-  dd=$(echo "$dt/86400" | bc)
-  dt2=$(echo "$dt-86400*$dd" | bc)
-  dh=$(echo "$dt2/3600" | bc)
-  dt3=$(echo "$dt2-3600*$dh" | bc)
-  dm=$(echo "$dt3/60" | bc)
-  ds=$(echo "$dt3-60*$dm" | bc)
 
-  printf " -- time elapsed: %d:%02d:%02d:%02.4f\n" $dd $dh $dm $ds
+  if [[ -x $(which bc) ]]; then
+    dt=$(echo "$ended_at - $started_at" | bc)
+    dd=$(echo "$dt/86400" | bc)
+    dt2=$(echo "$dt-86400*$dd" | bc)
+    dh=$(echo "$dt2/3600" | bc)
+    dt3=$(echo "$dt2-3600*$dh" | bc)
+    dm=$(echo "$dt3/60" | bc)
+    ds=$(echo "$dt3-60*$dm" | bc)
+
+    printf " -- time elapsed: %d:%02d:%02d:%02.4f\n" $dd $dh $dm $ds
+  else
+    warn "START: $started_at"
+    warn "END: $ended_at"
+  fi
 }
 
 
