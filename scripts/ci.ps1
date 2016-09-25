3cf7c643ec89eed8d7d43046bb27f120dfa63899
Anthony Cook <github@anthonymcook.com>
2016-09-25 18:02:24 -0400

    Add check Go build success.
    Intentionally breaking something for science.
    
    Re-enabling error highlighting.
    
    Revert "Re-enabling error highlighting."
    
    This reverts commit d333e33a037c6d35eef2669e8019cbe939b5ddbb.
    
    Revert "Intentionally breaking something for science."
    
    This reverts commit c3a835689ce1053854105ff177c6c6e5b2722cb3.

--- scripts/ci.ps1
+++ scripts/ci.ps1
@@ -1,26 +1,30 @@
 #!/usr/bin/env powershell
 
-Write-Host " -- OHAI"
-Write-Host " -- Meesa build yousa blacklight!"
 
 $blacklight = "blacklight.exe"
+Write-Host " -- building $blacklight"
+
 $srcfiles = (Get-ChildItem -Path "src\*.go" -Recurse).FullName
 go build -v -x -o "$blacklight" $srcfiles
 
-Write-Host " -- Weesa running blacklight programs now!"
+if ( ! ($LastExitCode -eq 0)) {
+  Write-Host " -- something went wrong!"
+  Write-Host " -- go was unable to build $blacklight"
+  exit 2
+}
 
 Get-ChildItem -Path "examples\*.bl" -exclude _* | % {
-  Write-Host " -- Running: " $_.FullName
-  $output = (echo "test" | & .\$blacklight $_.FullName 2>&1)
-  $blexit = $LastExitCode
-  Write-Host $output
-  # FIXME: verify program output vs expected
+  Write-Host " -- running: " $_.FullName
+    $output = (echo "test" | & .\$blacklight $_.FullName 2>&1)
+    $blexit = $LastExitCode
+    Write-Host $output
+# FIXME: verify program output vs expected
 
-  if ( ! ($blexit -eq 0)) {
-    Write-Host " -- Encountered error!"
-    exit 1
-  }
+    if ( ! ($blexit -eq 0)) {
+      Write-Host " -- something went wrong!"
+      exit 1
+    }
 }
 
 rm $blacklight
-Write-Host " -- All done!"
+Write-Host " -- done"
