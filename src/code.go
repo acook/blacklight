afcc69c715fe11ae2fe696b6327c52198622ccb1
Anthony Cook <github@anthonymcook.com>
2016-11-15 19:16:37 -0500

    Begin to improve compiler errors.

--- src/code.go
+++ src/code.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"runtime"
+	"strconv"
+)
+
 type Source struct {
 	filename  string
 	code      []rune
@@ -12,4 +17,17 @@ func NewSource(filename string) *Source {
 	source.filename = filename
 	source.sourcemap = make(map[int]int)
 	return source
+}
+
+func GoDebug(offset int) (file string, line string) {
+	_, file, lineno, ok := runtime.Caller(1)
+
+	if ok {
+		line = strconv.Itoa(lineno + offset)
+	} else {
+		file = "<unknown>"
+		line = "<unknown>"
+	}
+
+	return file, line
 }
