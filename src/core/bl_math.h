5577c9857b2fca7cbf5fb81de5378072d52d87f4
Anthony Cook <github@anthonymcook.com>
2018-05-10 23:36:31 -0400

    core: whitespace changes to min/max macros

--- src/core/bl_math.h
+++ src/core/bl_math.h
@@ -1,12 +1,9 @@
-#define min(a,b) ({ \
-  __typeof__ (a) _a = (a); \
+#define min(a,b) \
+  ({ __typeof__ (a) _a = (a); \
   __typeof__ (b) _b = (b); \
-  (void) (&_a == &_b); \
-  _a < _b ? _a : _b; \
-})
-#define max(a,b) ({ \
-  __typeof__ (a) _a = (a); \
+  _a < _b ? _a : _b; })
+
+#define max(a,b) \
+  ({ __typeof__ (a) _a = (a); \
   __typeof__ (b) _b = (b); \
-  (void) (&_a == &_b); \
-  _a > _b ? _a : _b; \
-})
+  _a > _b ? _a : _b; })
