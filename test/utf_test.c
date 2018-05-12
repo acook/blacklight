710e408ba0a83a8976334bc99cde415aea65877a
Anthony Cook <github@anthonymcook.com>
2018-05-12 05:27:58 -0400

    test: bring UTF8 test into line with new sumo impl

--- test/utf_test.c
+++ test/utf_test.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include "../ext/experimental/lib/files.h"
-#include "../ext/experimental/lib/sumo.h"
+#include "../src/core/files.h"
+#include "../src/core/sumo_string.h"
 #include "../ext/utf8.h/utf8.h"
 
 static inline void usage(void) { puts("usage: redlight filename.bl"); }
 
 int main(int argc, char *argv[]) {
   if (
-      argc != 2 || 
+      argc != 2 ||
       utf8cmp(argv[1], "--help") == 0 ||
       utf8cmp(argv[1], "-h") == 0) {
     usage();
     return 0;
   }
 
-  Sumo filename = sumo_new(utf8size(argv[1]));
+  sumo filename = sumo_new_prealloc(utf8size(argv[1]));
   utf8cpy(filename, argv[1]);
 
   int64_t filesize = fsize(filename);
@@ -24,19 +24,20 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  Sumo contents = sumo_new(filesize);
+  sumo contents = sumo_new_prealloc(filesize);
 
   FILE *file = fopen(filename, "rb");
   fread(contents, filesize, 1, file);
   contents[filesize] = 0x00; // terminate string in case the last line of the file isn't blank
 
-  printf("file size: %d\n", utf8size(contents));
+  printf("file size: %lu\n", utf8size(contents));
   printf("file contents:\n%s\n", contents);
 
-  Sumo cursor = sumo_cursor(contents, (bl_uint32){.i=100}); // grab a cursor in the middle of contents
-  
-  printf("file size: %d\n", utf8size(cursor));
-  printf("file contents:\n%s\n", cursor);
+  cursor c = sumo_cursor_new(contents); // grab a cursor in the middle of contents
+  c = sumo_cursor_mv(contents, c, 100);
+
+  printf("file size: %lu\n", utf8size(c));
+  printf("file contents:\n%s\n", c);
 
   return 0;
 }
