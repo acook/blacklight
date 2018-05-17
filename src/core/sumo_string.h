7304aae0477d71b6bd5d31cda6fe3ee9fe38c829
Anthony Cook <github@anthonymcook.com>
2018-05-17 01:28:41 -0400

    core: using bl_size instead of size_t, plus formatting ws

--- src/core/sumo_string.h
+++ src/core/sumo_string.h
@@ -13,8 +13,8 @@ typedef struct {
   uint32_t cap;
 } sumo_header;
 
-static inline sumo sumo_new_prealloc(size_t cap) {
-  size_t real_cap = cap + sizeof(sumo_header);  // make space for header
+static inline sumo sumo_new_prealloc(bl_size cap) {
+  bl_size real_cap = cap + sizeof(sumo_header);  // make space for header
 #ifdef JEMALLOC_C_
   sumo s = aligned_alloc(real_cap, sizeof(sumo_header));
 #else
@@ -22,30 +22,22 @@ static inline sumo sumo_new_prealloc(size_t cap) {
   // doesn't expose aligned_alloc or memalign so we fall back to malloc
   sumo s = malloc(real_cap);
 #endif
-  if (!s) return NULL; // unable to allocate memory
-  sumo_header *h = (void*)s;
+  if (!s) return NULL;  // unable to allocate memory
+  sumo_header* h = (void*)s;
   h->len = 0;
   h->cap = cap;
   return s;
 }
 
-static sumo sumo_new() {
-  return sumo_new_prealloc(0);
-}
+static sumo sumo_new() { return sumo_new_prealloc(0); }
 
-static size_t sumolen(sumo s){
-  return ((sumo_header*)s)->len;
-}
+static bl_size sumolen(sumo s) { return ((sumo_header*)s)->len; }
 
-static size_t sumocap(sumo s) {
-  return ((sumo_header*)s)->cap;
-}
+static bl_size sumocap(sumo s) { return ((sumo_header*)s)->cap; }
 
-static size_t sumo_sizeof(sumo s) {
-  return sumocap(s) + sizeof(sumo_header);
-}
+static bl_size sumo_sizeof(sumo s) { return sumocap(s) + sizeof(sumo_header); }
 
-static sumo sumo_resize(sumo s, size_t len) {
+static sumo sumo_resize(sumo s, bl_size len) {
   // FIXME: use rallocx to do aligned_realloc when jemalloc available
   sumo s2 = realloc(s, len + sizeof(sumo_header));
   if (s2) {
@@ -55,51 +47,50 @@ static sumo sumo_resize(sumo s, size_t len) {
   return s;  // unable to cat due to insufficient allocation
 }
 
-static sumo sumo_grow(sumo s, size_t len) {
+static sumo sumo_grow(sumo s, bl_size len) {
   if (sumocap(s) <= len) {
     return sumo_resize(s, len);
   }
-  return s;  // resize unneccessary because requested is less than actual
+  return s;  // resize unnecessary because requested is less than actual
 }
 
-static void sumocpy_unsafe(sumo dest, sumo src, size_t len) {
+static void sumocpy_unsafe(sumo dest, sumo src, bl_size len) {
   memcpy(dest + sizeof(sumo_header), src + sizeof(sumo_header), len);
 }
 
-static size_t sumocpy(sumo dest, sumo src, size_t len) {
-  size_t src_len = sumolen(src);
+static bl_size sumocpy(sumo dest, sumo src, bl_size len) {
+  bl_size src_len = sumolen(src);
   if (src_len < len) len = src_len;
-  size_t dest_cap = sumocap(dest);
+  bl_size dest_cap = sumocap(dest);
   if (dest_cap < len) len = dest_cap;
   if (len) sumocpy_unsafe(src, dest, len);
   return len;
 }
 
-static cursor sumo_cursor_new(sumo s) {
-  return s + sizeof(sumo_header);
-}
+static cursor sumo_cursor_new(sumo s) { return s + sizeof(sumo_header); }
 
-static cursor sumo_cursor_mv(sumo s, cursor c, size_t change) {
-  if (((c + change) > (sumo_cursor_new(s))) && ((c + change) < (s + sumo_sizeof(s)))) {
+static cursor sumo_cursor_mv(sumo s, cursor c, bl_size change) {
+  if (((c + change) > (sumo_cursor_new(s))) &&
+      ((c + change) < (s + sumo_sizeof(s)))) {
     return c + change;
   }
-  return c; // unable to move cursor to desired position
+  return c;  // unable to move cursor to desired position
 }
 
-static size_t sumo_cursor_len(sumo s, cursor c) {
+static bl_size sumo_cursor_len(sumo s, cursor c) {
   return (sumolen(s) + sizeof(sumo_header)) - (c - s);
 }
 
-static size_t sumo_cursor_pos(sumo s, cursor c) {
+static bl_size sumo_cursor_pos(sumo s, cursor c) {
   return c - sumo_cursor_new(s);
 }
 
 static sumo sumocat(sumo dest, sumo src) {
-  size_t end = sumocap(dest);
-  size_t len = sumolen(src) + sumolen(dest);
+  bl_size end = sumocap(dest);
+  bl_size len = sumolen(src) + sumolen(dest);
 
   sumo dest2 = sumo_grow(dest, len);
-  if (dest2)  {
+  if (dest2) {
     memcpy(sumo_cursor_new(dest2) + end, sumo_cursor_new(src), sumolen(src));
     ((sumo_header*)dest2)->len = len;
     return dest2;
@@ -109,8 +100,8 @@ static sumo sumocat(sumo dest, sumo src) {
 }
 
 static sumo sumocat_str(sumo dest, const char* src) {
-  size_t clen = strlen(src);
-  size_t new_len = clen + sumolen(dest);
+  bl_size clen = strlen(src);
+  bl_size new_len = clen + sumolen(dest);
 
   sumo dest2 = sumo_grow(dest, new_len);
   if (dest2) {
@@ -124,15 +115,15 @@ static sumo sumocat_str(sumo dest, const char* src) {
 
 static char* sumo_to_cstr(sumo s) {
   cursor c = sumo_cursor_new(s);
-  size_t len = sumo_cursor_len(s, c);
+  bl_size len = sumo_cursor_len(s, c);
   char* str = malloc(len + 1);
   memcpy(str, c, len);
   str[len] = 0x00;  // null-terminate string
   return str;
 }
 
-static char* sumo_cstrify(sumo s) {
-  sumo_header *h = (void*)s;
+static char* sumo_as_cstr(sumo s) {
+  sumo_header* h = (void*)s;
   cursor c = sumo_cursor_new(s);
   size_t len = sumolen(s);
   if (!c[len] || !c[len-1]) {
