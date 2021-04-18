e2baace5e61f7b32b59e88e97ff2ba20b06fbef7
Anthony M. Cook <github@anthonymcook.com>
2021-04-18 05:59:37 -0500

    Don't panic, return error for wrong arg
    This is a kind of WIP commit.
    
    It runs but there's some odd behvaiour documented in vm.go's handle()
    function.
    
    Basically, I'm seeing handle() run twice when a nested block encounters
    a TypeAssertionError. And as a result, the displayed error is correct
    but the VM state is wrong (because the error was caused by a different
    VM).
    
    I'm sure it's something obvious that I'll figure out tomorrow.
    
    In any case, this begins to bring runtime errors in-house and provide
    better context for them, even if the sourcemap isn't available.

--- src/vm.go
+++ src/vm.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 func doVM(bc []byte) (*VMstate, error) {
@@ -46,7 +48,12 @@ func coBC(label string, stack *Stack, bc []byte) {
 }
 
 func run_vm(vm *VMstate) (err error) {
-	defer handle(vm)
+	err = NewErr("VM did some shit", T("broke AF, handler not run"))
+	defer func() {
+		if ex := recover(); ex != nil {
+			err = handle(vm, ex)
+		}
+	}()
 
 	for {
 		vm.b = vm.bc[vm.o]
@@ -78,27 +85,44 @@ func run_vm(vm *VMstate) (err error) {
 			b := fmt.Sprintf("0%X ", vm.b)
 			print(b, "\n")
 			err = NewErr("vm: unrecognized bytecode at offset # "+o+": "+b, T(b))
-			return
+			return err
 		}
 
 		vm.o++
 		if vm.o >= vm.l {
-			return
+			return nil
 		}
 	}
 }
 
-func handle(vm *VMstate) {
-	if err := recover(); err != nil {
-		warn("(run_vm) something went terribly wrong: ")
-		switch err.(type) {
-		case *runtime.TypeAssertionError:
-			warn(err.(*runtime.TypeAssertionError).Error())
-		}
+func handle(vm *VMstate, ex interface{}) error {
+	warn("runtime error encountered in VM:")
+	switch ex.(type) {
+	case *runtime.TypeAssertionError:
+		msg := ex.(*runtime.TypeAssertionError).Error()
 
 		print("\n")
 		vm.debug()
 		print("\n")
-		panic(err)
+
+		// if a sub-block panics, this might get run, in which case it will return info about the last known bytecode
+		// processed by THIS VM, rather than the original VM that the error actually occured on
+		//
+		// it is very likely for vm.o to be at the end of the file, thus `code` will be 0-length
+		// infer will probably show the correct opword at the end of the file,
+		// but it won't be the opword which triggered the error
+		word := vm.infer(vm.b)
+		foff := len(strings.Split(B(vm.bc[0:vm.o]).PP(), " ")) - 1
+		code := B(vm.bc[vm.o : len(vm.bc)-1])
+		if len(code) > 0 {
+			str := code.PP()
+			word = strings.Split(str, " ")[0]
+		}
+
+		return NewErr("VM-ARG", T("opword #"+strconv.Itoa(foff)+" \""+word+"\" expected a different item on the @: "+msg))
+	default:
+		panic(ex)
 	}
+
+	return nil
 }
