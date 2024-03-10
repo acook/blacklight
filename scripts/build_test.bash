1471e7eefb39bf396e9d8910fd7b694445ccd913
Anthony Cook <github@anthonymcook.com>
2024-03-10 06:09:16 -0500

    Capture output of Go compilation in build script

--- scripts/build_test.bash
+++ scripts/build_test.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source "$(dirname ${BASH_SOURCE[0]})/_shared.bash"
+source "$(dirname "${BASH_SOURCE[0]}")/_shared.bash"
 
 timestamp="$(date --utc "+%Y.%m.%d")"
 shortsha="$(git rev-parse --short HEAD)"
@@ -8,9 +8,10 @@ blacklight="$BLROOT/bin/blacklight_$timestamp-$shortsha"
 
 warn " -- building blacklight binary..."
 go build -o "$blacklight" "$BLROOT/src/"*.go
+exitstatus=$?
 
-if [[ -x $blacklight ]]; then
-  warn " -- binary built: $(basename $blacklight)"
+if [[ $exitstatus == 0 && -x $blacklight ]]; then
+  warn " -- binary built: $(basename "$blacklight")"
   
   file "$blacklight" >&2
   ls -sh "$blacklight" >&2
@@ -19,7 +20,8 @@ if [[ -x $blacklight ]]; then
   echo "$blacklight"
 else
   warn " -- something went wrong!"
+  warn " -- go exit status: $exitstatus"
   warn " -- binary not found at: \"$blacklight\""
-  exit -1
+  exit 255
 fi
 
